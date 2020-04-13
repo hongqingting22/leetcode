f3ad93503ba138d20eda32c2091188e57e6e6904
hongqingting22 <lixl_email@163.com>
2020-04-13 10:24:13 +0800

    '最长上升子序列'

---
+++ leetcode/src/linkList/ReverseLink_92.java
@@ -0,0 +1,43 @@
+package linkList;
+//反转从位置 m 到 n 的链表。请使用一趟扫描完成反转。
+//
+// 说明:
+//1 ≤ m ≤ n ≤ 链表长度。
+//
+// 示例:
+//
+// 输入: 1->2->3->4->5->NULL, m = 2, n = 4
+//输出: 1->4->3->2->5->NULL
+// Related Topics 链表
+
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+//leetcode submit region end(Prohibit modification and deletion)
+
+public class ReverseLink_92 {
+   /* public ListNode reverseBetween(ListNode head, int m, int n) {
+        ListNode node = head;
+        ListNode pre = node;
+        ListNode first,last,next;
+        pre.next = node;
+        for(int i = 0;i<=n;i++){
+            if(m == i){
+                first = node;
+            }else if(i > m){
+
+            }
+            node = node.next;
+        }
+        pre.next = last;
+        first.next = next;
+    }*/
+}
